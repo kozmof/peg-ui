5ab4a287bc6327fddfb9be1f75f0e729f96cc282
Kozo Oeda <kozo.oeda@gmail.com>
2025-11-09 19:46:04 +0900

    Scope preflight on Storybook

--- panda.config.ts
+++ panda.config.ts
@@ -1,25 +1,32 @@
-import { defineConfig } from "@pandacss/dev";
-import { buttonRecipe} from './src/lib/Button/Button.recipe'
+import { defineConfig } from '@pandacss/dev';
+import { buttonRecipe } from './src/lib/Button/Button.recipe';
+
+const isStorybook = process.env.npm_lifecycle_event === 'storybook';
 
 export default defineConfig({
-  // Whether to use css reset
-  preflight: true,
+	// Whether to use css reset
+	preflight: isStorybook
+		? {
+        // Scope to avoid resetting CSS of a toolbar of preview
+				scope: '.sb-show-main'
+			}
+		: true,
 
-  // Where to look for your css declarations
-  include: ["./src/**/*.{js,svelte}", "./pages/**/*.{js,svelte}"],
+	// Where to look for your css declarations
+	include: ['./src/**/*.{js,svelte}', './pages/**/*.{js,svelte}'],
 
-  // Files to exclude
-  exclude: [],
+	// Files to exclude
+	exclude: [],
 
-  // Useful for theme customization
-  theme: {
-    extend: {
-      recipes: {
-        button: buttonRecipe
-      }
-    },
-  },
+	// Useful for theme customization
+	theme: {
+		extend: {
+			recipes: {
+				button: buttonRecipe
+			}
+		}
+	},
 
-  // The output directory for your css system
-  outdir: "styled-system",
+	// The output directory for your css system
+	outdir: 'styled-system'
 });
